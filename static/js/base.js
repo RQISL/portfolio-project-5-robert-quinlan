0a8078165664d2b81f0bc96b12df4e7af4a9a7cf
Robert Quinlan <creativewb50@gmail.com>
2023-10-09 19:21:08 +0000

    add select button, some modified css

---
+++ static/js/base.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+        $(".dropdown_btn").click(function () {
+            $(".slidedown").slideToggle("slow");
+        });
+    });
+
+    $(document).ready(function () {
+        $(".button_dropdown").click(function () {
+            $(".slidedown_gallery").slideToggle("slow");
+        });
+    });
