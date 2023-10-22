86a8cf06e866f9d6f242de1b4c2d19ab568490c3
Robert Quinlan <creativewb50@gmail.com>
2023-10-22 16:48:40 +0000

    removed workspace and make update to new workspace

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-u475b7es264.ws-eu105.gitpod.io', 'art-gallery-elena-d4d9090ab15b.herokuapp.com']
+ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-gldluwa65e5.ws-eu105.gitpod.io', 'art-gallery-elena-d4d9090ab15b.herokuapp.com']
 
 
 # Application definition
