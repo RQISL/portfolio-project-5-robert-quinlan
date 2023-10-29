add33923e78b709f6eafe3973271459c325a652f
Robert Quinlan <creativewb50@gmail.com>
2023-10-29 20:27:56 +0000

    Add model, view, admin, html and modified style

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class HomePage(models.Model):
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    image = CloudinaryField('image')
+    
+    def __str__(self):
+        return self.name
+
+
+class ElenaInfo(models.Model):
+    head = models.CharField(max_length=254)
+    info = models.TextField()
+
+    def __str__(self):
+        return self.head
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import HomePage, ElenaInfo
 
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    home_page = HomePage.objects.get()
+    elena_info = ElenaInfo.objects.get()
+    
+    return render(request, 'home/index.html', {'home_page': home_page, 'elena_info': elena_info})
