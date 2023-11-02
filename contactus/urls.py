c229ad6f1d747c037647def1676f31bb3e78facd
Robert Quinlan <creativewb50@gmail.com>
2023-11-02 21:54:19 +0000

    Deleted contactus app due errors

--- contactus/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.contact_us, name='contactus'),
-    path('hireme/<int:id>', views.hireme, name='hireme'),
-]
