b24465dd13e01367f659db4f6e43ef343f157251
Robert Quinlan <creativewb50@gmail.com>
2023-11-05 21:19:56 +0000

    Replace from home to home_page app due errors

--- home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import HomePage, ElenaInfo
-
-def index(request):
-    """ A view to return the index page """
-    home_page = HomePage.objects.get()
-    elena_info = ElenaInfo.objects.get()
-    
-    return render(request, 'home/index.html', {'home_page': home_page, 'elena_info': elena_info})
