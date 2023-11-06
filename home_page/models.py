ac98915a1567633358ef06b5140f04356a0c8487
Robert Quinlan <creativewb50@gmail.com>
2023-11-06 19:20:47 +0000

    modified to make correct syntax

--- home_page/models.py
+++ home_page/models.py
@@ -6,14 +6,14 @@ from cloudinary.models import CloudinaryField
 class HomePage(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    image = CloudinaryField('image')
     head = models.CharField(max_length=254)
     info = models.TextField()
+    image = CloudinaryField('image')
     
     def __str__(self):
         return self.name
 
     def get_name(self):
-        return self.name
+        return self.title
 
     
