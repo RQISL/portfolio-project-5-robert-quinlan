dcaec5dd500f446dc980e069b89a191ad134edc8
Robert Quinlan <creativewb50@gmail.com>
2023-11-08 23:36:39 +0000

    changed profiles to user_profile app

--- art_gallery/urls.py
+++ art_gallery/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('gallery_bag/', include('gallery_bag.urls')),
     path('search_product/', include('search_product.urls')),
     path('checkout/', include('checkout.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('user_profile/', include('user_profile.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
