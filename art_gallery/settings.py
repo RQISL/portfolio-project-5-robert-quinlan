b9683da83efbbcbd8c94295c0cf23d42fe9db2c6
Robert Quinlan <creativewb50@gmail.com>
2024-02-07 19:48:22 +0000

    Changed set to True for deployed

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-bux9ofh4b04.ws-eu108.gitpod.io', 'art-gallery-elena-d4d9090ab15b.herokuapp.com']
 
