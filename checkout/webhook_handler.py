00a3cfa98f303c821a77e87cf584da9b8e7fcd7f
Robert Quinlan <creativewb50@gmail.com>
2024-02-16 12:17:37 +0000

    Copy and paste in my previous work

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -10,7 +10,6 @@ from profiles.models import UserProfile
 import json
 import time
 
-
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
@@ -67,12 +66,12 @@ class StripeWH_Handler:
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
                 profile.default_phone_number = shipping_details.phone
-                profile.default_country = shipping_details.address.country
-                profile.default_postcode = shipping_details.address.postal_code
-                profile.default_town_or_city = shipping_details.address.city
                 profile.default_street_address1 = shipping_details.address.line1
                 profile.default_street_address2 = shipping_details.address.line2
+                profile.default_town_or_city = shipping_details.address.city
                 profile.default_county = shipping_details.address.state
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
                 profile.save()
 
         order_exists = False
@@ -83,12 +82,12 @@ class StripeWH_Handler:
                     full_name__iexact=shipping_details.name,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
-                    town_or_city__iexact=shipping_details.address.city,
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
+                    town_or_city__iexact=shipping_details.address.city,
                     county__iexact=shipping_details.address.state,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
                     grand_total=grand_total,
                     original_bag=bag,
                     stripe_pid=pid,
@@ -111,12 +110,12 @@ class StripeWH_Handler:
                     user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
-                    country=shipping_details.address.country,
-                    postcode=shipping_details.address.postal_code,
-                    town_or_city=shipping_details.address.city,
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
+                    town_or_city=shipping_details.address.city,
                     county=shipping_details.address.state,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
                     original_bag=bag,
                     stripe_pid=pid,
                 )
