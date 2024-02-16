674349113c41827abbd15938a6ea321f556c3536
Robert Quinlan <creativewb50@gmail.com>
2024-02-16 16:59:55 +0000

    Set to false

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-v66rodeu55s.ws-eu108.gitpod.io', 'art-gallery-elena-d4d9090ab15b.herokuapp.com']
 
