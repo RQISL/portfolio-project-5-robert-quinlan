96bb594fa7b67b06363217402ae0fad658746705
Robert Quinlan <creativewb50@gmail.com>
2024-02-16 14:33:35 +0000

    fix indent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -52,8 +52,9 @@ class StripeWH_Handler:
         save_info = intent.metadata.save_info
 
         # Get the Charge object
-        stripe_charge = stripe.Charge.retrieve(
-        intent.latest_charge
+        stripe_charge = stripe.Charge.retrieve
+        (
+            intent.latest_charge
         )
 
         billing_details = stripe_charge.billing_details  # updated
