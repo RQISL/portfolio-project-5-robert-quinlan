633e82d2d5f88334be06c5fecf6006c4a96843b3
Robert Quinlan <creativewb50@gmail.com>
2024-03-02 13:45:38 +0000

    Fix issue with 'Edit about us page' button

--- about_page/views.py
+++ about_page/views.py
@@ -22,7 +22,7 @@ def aboutus(request):
 
 @login_required
 def edit_about_page(request):
-    """ Edit a home personal page in the store """
+    """ Edit About Us page """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('aboutus'))
@@ -32,17 +32,17 @@ def edit_about_page(request):
         form = AboutPageForm(request.POST, request.FILES, instance=about_page)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully updated your home',
-                             'personal page!')
+            messages.success(request, 'Successfully updated your about us'
+                                      'in the page')
             return redirect(reverse('aboutus'))
         else:
             messages.error(request, 'Failed to update your personal page.',
-                           'Please ensure the form is valid.')
+                                    'Please ensure the form is valid.')
     else:
         form = AboutPageForm(instance=about_page)
         messages.info(request, f'You are editing {about_page.title}')
 
-    template = 'about_us/edit_about_page.html'
+    template = 'aboutus/edit_about_page.html'
     context = {
         'form': form,
         'about_page': about_page,
